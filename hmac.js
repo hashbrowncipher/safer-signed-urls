19b52a471ebcba5aa5449d8b7c7a1c54d1e98e4a
Josh Snyder <josh@code406.com>
2022-07-10 02:35:52 -0700

    Don't fail the viewer-request if cookie is missing

--- hmac.js
+++ hmac.js
@@ -2,11 +2,13 @@ var crypto = require('crypto');
 
 function handler(event) {
     var request = event.request;
-    var secret = request.cookies["cf-secret"].value;
-    
-    var hmac = crypto.createHmac("sha256", secret);
-    hmac.update(request.uri.split("?")[0]);
-    request.headers["cf-auth"] = { value: hmac.digest("base64") }
+    var cookies = request.cookies;
+    if(cookies.hasOwnProperty("cf-secret")) {
+      var secret = cookies["cf-secret"].value;
+      var hmac = crypto.createHmac("sha256", secret);
+      hmac.update(request.uri.split("?")[0]);
+      request.headers["cf-auth"] = { value: hmac.digest("base64") }
+    }
 
     return request;
 }
